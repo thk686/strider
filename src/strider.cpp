a5919e662ef684fe73986102c70814ac30eaf57b
Tim Keitt <tkeitt@gmail.com>
2017-11-18 09:35:16 -0600

    added convolve2

--- src/strider.cpp
+++ src/strider.cpp
@@ -11,6 +11,7 @@ using std::begin;
 using std::end;
 #include <strider.h>
 using strider::make_strided;
+using strider::make_strided_range;
 
 //' Fast row sums
 //' 
@@ -27,6 +28,7 @@ using strider::make_strided;
 //' row_sums(matrix(1:9, 3))
 //' col_sums(matrix(1:9, 3))
 //' 
+//' @seealso \code{\link{rowSums}}, \code{\link{colSums}}
 //' @rdname sums
 //' @export
 // [[Rcpp::export]]
@@ -53,5 +55,51 @@ NumericVector col_sums(const NumericMatrix& x)
   transform(make_strided(begin(x), nr), make_strided(end(x), nr), begin(res),
             [&](const double& v){ return accumulate(&v, &v + nr, 0.0); });
   return res;
+}
+
+//' Convolve Matrices
+//' 
+//' Demonstration of fast matrix convolution C++
+//' 
+//' @param a a numeric matrix
+//' @param b a numeric matrix
+//' 
+//' @details A very efficient matric convolution implementation that demonstrates
+//' the use of the strided pointer and strided range concepts. Performance will be
+//' improved if the smaller matrix is given as the second argument.
+//'
+//' @examples
+//' a = matrix(c(1, 2, 1,
+//'              1, 1, 1), 2, 3, byrow = TRUE)
+//' b = matrix(c(0, 0, 0,
+//'              0, 0, 0,
+//'              0, 1, 0,
+//'              0, 0, 0), 4, 3, byrow = TRUE)
+//' convolve2(a, b)
+//' 
+//' @seealso \code{\link{convolve}}
+//' @rdname convolve
+//' @export
+// [[Rcpp::export]]
+NumericMatrix
+convolve2(const NumericMatrix& a,
+          const NumericMatrix& b)
+{
+  int
+  nra = a.nrow(), nca = a.ncol(),
+    nrb = b.nrow(), ncb = b.ncol(),
+    nrab = nra + nrb - 1,
+    ncab = nca + ncb - 1;
+  NumericMatrix ab(nrab, ncab);
+  auto acc1 = make_strided(begin(ab), nrab);
+  for (auto&& t : make_strided_range(begin(a), nra, nca))    {
+    auto acc2 = make_strided(&*acc1++, nrab);
+    for (auto&& u : make_strided_range(begin(b), nrb, ncb))  {
+      auto acc3 = &*acc2++;
+      for (auto&& v : make_strided_range(&t, 1, nra))        {
+        auto acc4 = acc3++;
+        for (auto&& w : make_strided_range(&u, 1, nrb))      {
+          *acc4++ += v * w;                                  }}}}
+  return ab;
 }
 
