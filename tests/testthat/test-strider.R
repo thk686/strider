e3c996f48a875480d409838fffcb56011f3ffce2
Tim Keitt <tkeitt@gmail.com>
2017-11-18 09:35:29 -0600

    convolve2

--- tests/testthat/test-strider.R
+++ tests/testthat/test-strider.R
@@ -10,3 +10,12 @@ test_that("col_sums are correct", {
   expect_equal(col_sums(x), colSums(x))
 })
 
+test_that("convolve2 is correct", {
+  a = matrix(c(1, 2, 1,
+               1, 1, 1), 2, 3, byrow = TRUE)
+  b = matrix(c(0, 0, 0,
+               0, 0, 0,
+               0, 1, 0,
+               0, 0, 0), 4, 3, byrow = TRUE)
+  expect_identical(t(convolve2(a, b)), convolve2(t(a), t(b)))
+})
