7479b98b45c4c4be33383ef115f6e64c2240b4f1
Tim Keitt <tkeitt@gmail.com>
2018-01-09 20:06:43 -0600

    stl_algo_convolve2

--- tests/testthat/test-strider.R
+++ tests/testthat/test-strider.R
@@ -11,11 +11,26 @@ test_that("col_sums are correct", {
 })
 
 test_that("convolve2 is correct", {
+  rconv2 = function(a, b)
+  {
+    ab = matrix(nrow = nrow(a) + nrow(b) - 1,
+                ncol = ncol(a) + ncol(b) - 1,
+                data = 0)
+    for (i in 1:ncol(a) - 1)
+      for (j in 1:ncol(b) - 1)
+        for (k in 1:nrow(a) - 1)
+          for (l in 1:nrow(b) - 1)
+            ab[k + l + 1, i + j + 1] =
+              ab[k + l + 1, i + j + 1] +
+              a[k + 1, i + 1] * b[l + 1, j + 1]
+    return(ab)
+  }
   a = matrix(c(1, 2, 1,
                1, 1, 1), 2, 3, byrow = TRUE)
   b = matrix(c(0, 0, 0,
                0, 0, 0,
                0, 1, 0,
                0, 0, 0), 4, 3, byrow = TRUE)
+  expect_identical(rconv2(a, b), convolve2(a, b))
   expect_identical(t(convolve2(a, b)), convolve2(t(a), t(b)))
 })
